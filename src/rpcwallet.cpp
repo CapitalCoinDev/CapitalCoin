1c5727d52bd6814abe8e7a96065944d22198118c
Brabo99 <Brabo99@users.noreply.github.com>
2014-07-04 03:49:48 +0000

    Update rpcwallet.cpp

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -74,8 +74,9 @@ Value getinfo(const Array& params, bool fHelp)
     obj.push_back(Pair("protocolversion",(int)PROTOCOL_VERSION));
     obj.push_back(Pair("walletversion", pwalletMain->GetVersion()));
     obj.push_back(Pair("balance", ValueFromAmount(pwalletMain->GetBalance())));
+    obj.push_back(Pair("newmint",       ValueFromAmount(pwalletMain->GetNewMint())));
+    obj.push_back(Pair("stake",         ValueFromAmount(pwalletMain->GetStake())));
     obj.push_back(Pair("blocks", (int)nBestHeight));
-    obj.push_back(Pair("timeoffset", (boost::int64_t)GetTimeOffset()));
     obj.push_back(Pair("moneysupply", ValueFromAmount(pindexBest->nMoneySupply)));
     obj.push_back(Pair("connections", (int)vNodes.size()));
     obj.push_back(Pair("proxy", (proxy.first.IsValid() ? proxy.first.ToStringIPPort() : string())));
